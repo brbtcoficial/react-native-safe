f51d3ff8abbc4b35d336aa0e87f61de8ad86334d
Erick Crus <erickcrus@gmail.com>
2024-09-27 20:10:20 -0300

    refactor: refactor vision camera ios plugin

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: [
+    ["react-native-worklets-core/plugin"],
     ['module:react-native-builder-bob/babel-preset', { modules: 'commonjs' }],
   ],
 };
--- ios/SafeFrameProcessor/VisionCameraPlugin.m
+++ ios/SafeFrameProcessor/VisionCameraPlugin.m
@@ -1,67 +1,20 @@
-#import <Foundation/Foundation.h>
-
 #import <VisionCamera/FrameProcessorPlugin.h>
 #import <VisionCamera/FrameProcessorPluginRegistry.h>
-#import <VisionCamera/VisionCameraProxy.h>
 #import <VisionCamera/Frame.h>
 
-#import <WebRTC/RTCMediaConstraints.h>
-#import <WebRTC/RTCConfiguration.h>
-#import <WebRTC/RTCIceServer.h>
-#import <react_native_webrtc/WebRTCModule.h>
-#import <react_native_webrtc/WebRTCModule+RTCPeerConnection.h>
 
-
-@interface WebRTCPluginPlugin : FrameProcessorPlugin
-@property(weak, nonatomic) WebRTCModule *module;
-@property(nonatomic, strong) RTCCameraVideoCapturer *capturer;
+@interface B8SafeFrameProcessorPlugin : FrameProcessorPlugin
 @end
 
-@implementation WebRTCPluginPlugin
-//- (void) connectRTC {
-//
-//  NSMutableArray<RTCIceServer *> *iceServers = [NSMutableArray new];
-//  [iceServers addObject:[[RTCIceServer alloc] initWithURLStrings: @[@"turn:54.94.8.152:3478"]
-//                                                        username: @"yJr4LWEp82m6gVCh"
-//                                                      credential: @"xrp73KVqagYMG2te"]];
-//
-//  RTCConfiguration *configuration = [[RTCConfiguration alloc] init];
-//  configuration.iceServers = iceServers;
-//
-//  RTCMediaConstraints *constraints = [[RTCMediaConstraints alloc] initWithMandatoryConstraints: @{
-//                                                                      @"width": @"1280",
-//                                                                      @"height": @"720",
-//                                                                      @"frameRate": @"23"
-//                                                                  }
-//                                                                           optionalConstraints:nil];
-//
-//  RTCPeerConnection *peerConnection = [self.module.peerConnectionFactory peerConnectionWithConfiguration:configuration
-//                                                                                        constraints:constraints
-//                                                                                           delegate:self.module];
-//  if (peerConnection == nil) {
-//      return;
-//  }
-//
-//  peerConnection.dataChannels = [NSMutableDictionary new];
-//  peerConnection.reactTag = @(0);
-//  peerConnection.remoteStreams = [NSMutableDictionary new];
-//  peerConnection.remoteTracks = [NSMutableDictionary new];
-//  peerConnection.webRTCModule = self.module;
-//
-//  self.module.peerConnections[@(0)] = peerConnection;
-//
-//  NSLog(peerConnection.connectionState ? @"YES" : @"NO");
-//};
+@implementation B8SafeFrameProcessorPlugin
 
-- (instancetype _Nonnull)initWithProxy:(VisionCameraProxyHolder*)proxy
-                           withOptions:(NSDictionary* _Nullable)options {
+- (instancetype) initWithProxy:(VisionCameraProxyHolder*)proxy
+                   withOptions:(NSDictionary* _Nullable)options {
   self = [super initWithProxy:proxy withOptions:options];
-//  [self connectRTC];
   return self;
 }
 
-- (id _Nullable)callback:(Frame* _Nonnull)frame
-           withArguments:(NSDictionary* _Nullable)arguments {
+- (id)callback:(Frame*)frame withArguments:(NSDictionary*)arguments {
   CVImageBufferRef buffer = CMSampleBufferGetImageBuffer(frame.buffer);
   
   CIImage *ciImage = [CIImage imageWithCVPixelBuffer:buffer];
@@ -72,16 +25,10 @@
   UIImage *image = [[UIImage alloc] initWithCGImage:videoImage];
   
   float scale = [arguments[@"resolutionMultiplier"] floatValue];
-//  int newWidth = image.size.width*scale;
-//  int newHeight = image.size.height*scale;
-//  UIGraphicsBeginImageContext(CGSizeMake(newWidth, newHeight));
-//  [image drawInRect:CGRectMake(0, 0, newWidth, newHeight)];
-//  UIImage *destImage = UIGraphicsGetImageFromCurrentImageContext();
-//  UIGraphicsEndImageContext();
   
   return [UIImageJPEGRepresentation(image, scale) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
 }
 
-VISION_EXPORT_FRAME_PROCESSOR(WebRTCPluginPlugin, webrtcPlugin)
+VISION_EXPORT_FRAME_PROCESSOR(B8SafeFrameProcessorPlugin, b8SafeProcessor)
 
 @end
