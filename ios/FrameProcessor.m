1f29d4a77a5e4c065cf5b633b68c50b4e2e089fc
Erick Crus <erickcrus@gmail.com>
2024-11-30 15:46:30 -0300

    fix(ios native): fix iOS frame processor bug

--- ios/FrameProcessor.m
+++ ios/FrameProcessor.m
@@ -14,18 +14,43 @@
 }
 
 - (id)callback:(Frame*)frame withArguments:(NSDictionary*)arguments {
-  CVImageBufferRef buffer = CMSampleBufferGetImageBuffer(frame.buffer);
+  float scale = 1;
+  if (arguments != nil && [arguments[@"resolutionMultiplier"] isKindOfClass:[NSNumber class]]) {
+    scale = [arguments[@"resolutionMultiplier"] floatValue];
+  } else {
+      @throw [NSException exceptionWithName:@"RuntimeException"
+                                    reason:@"resolutionMultiplier property has to be a number!"
+                                  userInfo:nil];
+  }
+  
+  CVPixelBufferRef buffer = CMSampleBufferGetImageBuffer(frame.buffer);
+  if (buffer == NULL) {
+    @throw [NSException exceptionWithName:@"RuntimeException"
+                                  reason:@"Failed to get image buffer from frame!"
+                                userInfo:nil];
+  }
   
   CIImage *ciImage = [CIImage imageWithCVPixelBuffer:buffer];
   CIContext *temporaryContext = [CIContext contextWithOptions:nil];
-  CGImageRef videoImage = [temporaryContext
-                              createCGImage:ciImage
-                           fromRect:CGRectMake(0, 0, CVPixelBufferGetWidth(buffer), CVPixelBufferGetHeight(buffer))];
+  CGImageRef videoImage = [temporaryContext createCGImage:ciImage
+                                                fromRect:CGRectMake(0, 0, CVPixelBufferGetWidth(buffer), CVPixelBufferGetHeight(buffer))];                                              
+  if (videoImage == NULL) {
+    @throw [NSException exceptionWithName:@"RuntimeException"
+                                  reason:@"Failed to create CGImage from CIImage!"
+                                userInfo:nil];
+  }
+
   UIImage *image = [[UIImage alloc] initWithCGImage:videoImage];
+  CGImageRelease(videoImage);
+
+  NSData *imageData = UIImageJPEGRepresentation(image, scale);
+  if (imageData == nil) {
+    @throw [NSException exceptionWithName:@"RuntimeException"
+                                  reason:@"Failed to create JPEG representation of image!"
+                                userInfo:nil];
+  }
   
-  float scale = [arguments[@"resolutionMultiplier"] floatValue];
-  
-  return [UIImageJPEGRepresentation(image, scale) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+  return [imageData base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
 }
 
 VISION_EXPORT_FRAME_PROCESSOR(B8SafeFrameProcessorPlugin, b8SafeProcessor)
